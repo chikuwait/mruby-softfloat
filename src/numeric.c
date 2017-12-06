55ef5e7174d9b2e2390c761483d42b857ab95410
chikuwait <chikuwa.1703@gmail.com>
2017-12-06 15:52:13 +0900

    replace flo_eql with softfloat

--- src/numeric.c
+++ src/numeric.c
@@ -335,7 +335,7 @@ flo_eql(mrb_state *mrb, mrb_value x)
 
   mrb_get_args(mrb, "o", &y);
   if (!mrb_float_p(y)) return mrb_false_value();
-  return mrb_bool_value(mrb_float(x) == (mrb_float)mrb_fixnum(y));
+  return mrb_bool_value(f64_eq(mrb_float(x),i64_to_f64(mrb_fixnum(y))));
 }
 
 /* 15.2.9.3.7  */
