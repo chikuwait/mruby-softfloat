817a099e2436e7db4f608f26fe9b0e49ae0be02a
chikuwait <chikuwa.1703@gmail.com>
2017-12-07 11:32:39 +0900

    replace mrb_fixnum_minus with softfloat

--- src/numeric.c
+++ src/numeric.c
@@ -1275,7 +1275,7 @@ mrb_fixnum_minus(mrb_state *mrb, mrb_value x, mrb_value y)
     b = mrb_fixnum(y);
     if (mrb_int_sub_overflow(a, b, &c)) {
 #ifndef MRB_WITHOUT_FLOAT
-      return mrb_float_value(mrb, (mrb_float)a - (mrb_float)b);
+      return mrb_float_value(mrb, f64_sub(i64_to_f64(a),i64_to_f64(b)));
 #endif
     }
     return mrb_fixnum_value(c);
@@ -1283,7 +1283,7 @@ mrb_fixnum_minus(mrb_state *mrb, mrb_value x, mrb_value y)
 #ifdef MRB_WITHOUT_FLOAT
   mrb_raise(mrb, E_TYPE_ERROR, "non fixnum value");
 #else
-  return mrb_float_value(mrb, (mrb_float)a - mrb_to_flo(mrb, y));
+  return mrb_float_value(mrb, f64_sub(i64_to_f64(a),mrb_to_flo(mrb, y)));
 #endif
 }
 
